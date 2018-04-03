bf4601691e8bd159e0e33ef2aa6e996b3dcda3f6
Nick Sweet <nick@silo.io>
2018-04-03 10:57:37 -0700

    Update BlueCryptor dependencie to v1.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 		)
 	],
 	dependencies: [
-		.package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "0.8.0"),
+		.package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.0"),
 	],
 	targets: [
 		.target(
